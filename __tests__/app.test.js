ffb5c5192d939edaabd9d97ca28132e8b1b031db
Chris Maylor <chrismaylor1@hotmail.com>
2023-11-20 16:33:42 +0000

    takes out test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -38,3 +38,23 @@ describe("GET /api/topics", () => {
         });
     });
 })
+
+describe("GET /api/articles", () => {
+    test("200 sends an array of articles to the client", () => {
+      return request(app)
+        .get("/api/articles")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.articles.length).toBe(13);
+          body.articles.forEach((article) => {
+            expect(typeof article.title).toBe("string");
+            expect(typeof article.topic).toBe("string");
+            expect(typeof article.author).toBe("string");
+            expect(typeof article.body).toBe("string");
+            expect(typeof article.created_at).toBe("string");
+            expect(typeof article.article_id).toBe("number");
+            expect(typeof article.article_img_url).toBe("string");
+          });
+        });
+    });
+})
