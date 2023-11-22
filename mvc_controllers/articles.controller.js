fa605609b25159d538b3ac8cc594963c674ceb9c
Chris Maylor <chrismaylor1@hotmail.com>
2023-11-22 08:54:01 +0000

    adds GET articles (topic query) endpoint with all tests

--- mvc_controllers/articles.controller.js
+++ mvc_controllers/articles.controller.js
@@ -1,17 +1,33 @@
 const {selectArticles} = require('../mvc_models/articles.model')
 const {selectArticleById} = require('../mvc_models/articles.model')
+const {checkTopicExists} = require('../mvc_models/topics.model')
 
 exports.getArticles = (req, res, next) => {
-    selectArticles()
+    const {query} = req
+    const queryKeys = Object.keys(query)
+    if(queryKeys.length > 0){
+        checkTopicExists(query.topic)
+        .then(() => {
+            return selectArticles(query)
+        })
+        .then((articles) => {
+            console.log('helloo')
+            res.status(200).send({articles})
+        })
+        .catch(next)
+    } else {
+    selectArticles(query)
     .then((articles) => {
-        console.log(articles.rows)
-        res.status(200).send({articles: articles.rows})
+        res.status(200).send({articles})
     })
+    .catch(next)
+}
 }
 
 exports.getArticleById = (req, res, next) => {
     selectArticleById(req.params.article_id)
     .then(({rows}) => {
+        console.log(rows)
         res.status(200).send({article: rows[0]})
     })
     .catch(next)
--- mvc_models/articles.model.js
+++ mvc_models/articles.model.js
@@ -1,19 +1,38 @@
 const db = require("../db/connection");
 
 
-exports.selectArticles = () => {
-    let articles 
-    return db.query(`
-    SELECT articles.title, articles.topic, articles.author, articles.created_at, articles.article_id, articles.article_img_url, 
-    COUNT (comment_id) AS comment_count 
-    FROM articles
-    LEFT JOIN comments
-    ON articles.article_id = comments.article_id
-    GROUP BY articles.article_id
-    ORDER BY created_at DESC;`)
+exports.selectArticles = (query) => {
+    if(query.topic){
+            return db.query(`
+            SELECT articles.title, articles.topic, articles.author, articles.created_at, articles.article_id, articles.article_img_url, 
+            COUNT (comment_id) AS comment_count 
+            FROM articles
+            LEFT JOIN comments
+            ON articles.article_id = comments.article_id
+            WHERE topic = $1
+            GROUP BY articles.article_id
+            ORDER BY created_at DESC;`, [query.topic])
+            .then((result) => {
+                return result.rows
+            }) 
+    } 
+    if (query.topic === undefined) {
+        return db.query(`
+        SELECT articles.title, articles.topic, articles.author, articles.created_at, articles.article_id, articles.article_img_url, 
+        COUNT (comment_id) AS comment_count 
+        FROM articles
+        LEFT JOIN comments
+        ON articles.article_id = comments.article_id
+        GROUP BY articles.article_id
+        ORDER BY created_at DESC;`)
+        .then((result) => {
+            return result.rows
+        })
+    }   
 }
 
 exports.selectArticleById = (id) => {
+    
         return db.query(`
         SELECT * FROM articles WHERE article_id = $1;`, [id])
         .then((result) =>{
@@ -31,6 +50,7 @@ exports.checkArticleExists = (id) => {
         SELECT * FROM articles WHERE article_id = $1;`, [id])
         .then((result) =>{
             if(result.rows.length === 0){
+                console.log('im in here')
                 return Promise.reject({status:404, msg: 'Article does not exist.'})
             }
         })
