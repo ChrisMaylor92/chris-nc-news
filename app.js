9fe54ec18ed74c5734581728a792978eb63bbe33
Chris Maylor <chrismaylor1@hotmail.com>
2023-11-22 10:59:57 +0000

    minor changes

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const app = express()
 app.use(express.json());
 
 
-app.get('/api', getEndpoints)
-app.get('/api/topics', getTopics)
-app.get('/api/articles', getArticles)
-app.get('/api/articles/:article_id', getArticleById)
-app.post('/api/articles/:article_id/comments', postComment)
+app.get('/api', getEndpoints)//yeah
+app.get('/api/topics', getTopics)//yeah
+app.get('/api/articles', getArticles)//yeah
+app.get('/api/articles/:article_id', getArticleById) //yeah
+app.post('/api/articles/:article_id/comments', postComment) //yeah
 app.get('/api/articles/:article_id/comments', getCommentsByArticleId)
 
 
